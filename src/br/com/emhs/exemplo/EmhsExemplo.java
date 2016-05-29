f385436e181e5d42c0813d12219f5b7800381f96
Francisco Silva <silva.fass@gmail.com>
2016-05-29 17:42:23 -0300

    Melhorias

--- src/br/com/emhs/exemplo/EmhsExemplo.java
+++ src/br/com/emhs/exemplo/EmhsExemplo.java
@@ -2,21 +2,21 @@ package br.com.emhs.exemplo;
 
 import java.io.IOException;
 
+import br.com.deployxtech.emhs.Emhs;
 import br.com.deployxtech.emhs.core.annotation.Service;
-import br.com.deployxtech.emhs.http.server.EmhsServer;
 
 @Service
 public class EmhsExemplo {
-	
+
 	public String hello() {
 		return "Hello World";
 	}
-	
+
 	public String hello(String nome, Integer idade) {
 		return "Ola "+nome+" sua idade é "+idade;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		new EmhsServer(8000).start();
+		Emhs.server(8000).start();
 	}
 }
